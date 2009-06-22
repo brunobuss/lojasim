03b22b25303f5e26b659dd9594cc6f1c76f018fb
Thiago Elias <e.thiago.06@gmail.com>
2009-06-22 04:15:30 +0000

    Blah2
    git-svn-id: http://lojasim.googlecode.com/svn/trunk@45 b48b944c-4e24-11de-a6a3-df26d2c71dbe

--- src/pedidoDispatcher.cpp
+++ src/pedidoDispatcher.cpp
@@ -77,6 +77,6 @@ void pedidoDispatcher::iniciaThreadReposicao(Pedido* p, Estoquista *e)
     pedidoThread *pedido = new pedidoThread(p,e);
     connect(pedido, SIGNAL(registerLog(QString)), this, SIGNAL(registerLog(QString)));
     connect(pedido, SIGNAL(registerLogCompra(logMessageCompra*)), this, SIGNAL(registerLogCompra(logMessageCompra*)));
-    connect(pedido, SIGNAL(finalizouPedido()), this, SLOT(retornaEstoquista()));
+    connect(pedido, SIGNAL(finalizouPedido(Estoquista *)), this, SLOT(retornaEstoquista(Estoquista *);));
     pedido->start();
 }
